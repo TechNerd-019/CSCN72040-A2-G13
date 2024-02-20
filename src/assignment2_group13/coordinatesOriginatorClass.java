5d65aa48e6cbcc25916d9d2421f47b859c562359
Komalpreet <kkomalpreet462@gmail.com>
2024-02-19 19:19:55 -0500

    Documentation

--- src/assignment2_group13/coordinatesOriginatorClass.java
+++ src/assignment2_group13/coordinatesOriginatorClass.java
@@ -1,21 +1,31 @@
 package assignment2_group13;
 
 import java.util.Arrays;
-
+/**
+ * The `coordinatesOriginatorClass` is responsible for creating and restoring states of
+ * its coordinates. It acts as the originator in the Memento design pattern, which allows
+ * for the state of an object to be saved and restored without breaking encapsulation.
+ */
 public class coordinatesOriginatorClass {
-	
+	/**
+     * The current state of coordinates.
+     */
 	private double coordinates[][];
-	
+	/**
+     * Sets the current coordinates state.
+     *
+     * @param coordinates A 2D array of doubles representing the coordinates.
+     */
 	public void setCoordinate(double coordinates[][])
 	{
 		this.coordinates = coordinates;
 	}
 	
-	/*
-	 * Now that we have created the Memento class
-	 * and configured our setters and getters accordingly,
-	 * we can save into the Memento.
-	 */
+	/**
+     * Saves the current coordinates state into a memento object.
+     * 
+     * @return A new instance of `locationCoordinatesMemento` containing the current state.
+     */
 	
 	public locationCoordinatesMemento storeInMemento()
 	{
@@ -23,7 +33,12 @@ public class coordinatesOriginatorClass {
 		return new locationCoordinatesMemento(coordinates);
 		
 	}
-	
+	/**
+     * Restores the coordinates state from a memento object.
+     * 
+     * @param memento The `locationCoordinatesMemento` object from which to restore the state.
+     * @return The restored coordinates as a 2D array of doubles.
+     */
 	public double[][] restoreFromMemento(locationCoordinatesMemento memento)
 	{
 		coordinates = memento.getCoordinates();
